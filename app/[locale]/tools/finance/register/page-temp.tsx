2dd02862074b29c790c34556183115cb9ad468ac
Caio Peixoto <caio.santos@havan.com.br>
2026-01-02 13:26:58 -0300

    fix: correcao de importacao

--- app/[locale]/tools/finance/register/page-temp.tsx
+++ app/[locale]/tools/finance/register/page-temp.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "@/lib/firebase";
-import { loginAction } from "../login/actions"; // Reusing the login action to set cookie
-import Link from "next/link";
-import { useTranslations } from "next-intl";
+import RegisterForm from "./RegisterForm";
 
 export default function RegisterPage({ params }: { params: { locale: string } }) {
     // NOTE: Simple unwrapping of params for client component is okay here if passed from server parent, 
